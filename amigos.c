6ef3b92277286adf6db9d1f93f0805dd66d068f2
Miguel Hernández <par9615@gmail.com>
2018-03-15 17:47:53 -0600

    MPI parallelized?

--- amigos.c
+++ amigos.c
@@ -1,27 +1,71 @@
 #include <stdio.h>
+#include "mpi.h"
 
-int divisors_sum(int n)
+#define SERVER 0
+
+int divisors_sum(int);
+int find_friend_numbers();
+void assign_tasks();
+
+int rank, nprocs;
+int left, right;
+
+
+int main(int argc, char *argv[])
 {
-	int sum = 0;
-	for(int i = 1; i < n; i++)
-		if(n%i == 0)
-			sum+=i;
+	int left, right;	  
+	
+	
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);	
 
-	return sum;
+
+	printf("Procesos %d\n",nprocs);
+	assign_tasks();
+	find_friend_numbers();
+
+	MPI_Finalize();
+
+	return 0;
 }
 
-int main()
+void assign_tasks()
 {
-	int l, r;
-	scanf("%d %d", &l, &r);
+	int msg[2];	
 
-	
+	if(rank == SERVER)
+	{
+		printf("Ingresa el rango en el que deseas buscar\n");
+		scanf("%d %d", &left, &right);
+		msg[0] = left;
+		msg[1] = right;
 
-	for(int i = l; i <= r; i++)
+		for(int i = 0; i < nprocs; i++)
+			if(i != rank)
+				MPI_Send(msg, 2, MPI_INT, rank, 0, MPI_COMM_WORLD);	
+	}
+
+	else
+	{
+		printf("CHALE\n");
+		MPI_Recv(msg, 2, MPI_INT, SERVER, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		left = msg[0] + rank;
+		right = msg[1];
+
+
+	}
+}
+
+int find_friend_numbers()
+{
+	printf("Buscando desde el proceso %d\n", rank);
+	printf("Desde %d hasta %d\n", left, right);
+	for(int i = left; i <= right; i+=nprocs)
 	{
 		int a = divisors_sum(i);
 
-		for(int j = i+1; j <= r; j++)
+		for(int j = i+1; j <= right; j++)
 		{
 			int b = divisors_sum(j);
 			if(i != j && a == j && b == i)
@@ -31,5 +75,16 @@ int main()
 			}
 		}
 	}
-	return 0;
+
+}
+
+
+int divisors_sum(int n)
+{
+	int sum = 0;
+	for(int i = 1; i < n; i++)
+		if(n%i == 0)
+			sum+=i;
+
+	return sum;
 }
